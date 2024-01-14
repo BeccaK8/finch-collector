1a946fe97d029addb4a16af031fbb2920e03e267
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2024-01-14 09:27:07 -0600

    show finch index page

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+finches = [
+    { 'name': 'Tweety Bird', 'breed': 'Canary', 'color': 'Yellow' },
+    { 'name': 'Big Red', 'breed': 'Cardinal', 'color': 'Red' },
+    { 'name': 'Newspaper', 'breed': 'Zebra Finch', 'color': 'Black and White' },
+]
 # Create your views here.
 # Render home view - '/'
 def home(request):
@@ -8,3 +13,9 @@ def home(request):
 # Render about page - '/about/'
 def about(request):
     return render(request, 'about.html')
+
+# Render finch index page - '/finches/'
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
