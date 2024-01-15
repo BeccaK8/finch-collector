cad682d4a4eb14b4a0661811939bd2beaaacb8dc
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2024-01-15 10:42:10 -0600

    create Finch model and setup superuser

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100, default=None)
+    breed = models.CharField(max_length=100, default=None)
+    description = models.TextField(max_length=250, default=None)
+    color = models.CharField(max_length=100, default=None)
+    size_inches = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
