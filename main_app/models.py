bc8dcfff12a5c794f32cb84e4d76114a3e876ca5
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2024-01-18 07:22:22 -0600

    add Feeder model and admin

--- main_app/models.py
+++ main_app/models.py
@@ -8,7 +8,32 @@ MEALS = (
     ('D', 'Dinner')
 )
 
+FOOD_TYPE = (
+    ('G', 'Grasses'),
+    ('I', 'Insects'),
+    ('L', 'Liquid'),
+    ('S', 'Seeds')
+)
+
 # Create your models here.
+class Feeder(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    food_type = models.CharField(
+        max_length=1,
+        choices=FOOD_TYPE,
+        default=FOOD_TYPE[2][0]
+    )
+
+    def __str__(self):
+        return f'{self.name} at {self.location}'
+    
+    def get_absolute_url(self):
+        return reverse('feeder_detail', kwargs={'pk': self.id})
+    
+    class Meta:
+        ordering = [ 'name' ]
+
 class Finch(models.Model):
     name = models.CharField(max_length=100, default=None)
     breed = models.CharField(max_length=100, default=None)
